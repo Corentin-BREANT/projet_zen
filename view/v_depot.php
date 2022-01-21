b06b0dee4153ae905664dc623d697486157908b0
Corentin-BREANT <84771683+Corentin-BREANT@users.noreply.github.com>
2022-01-21 15:56:08 +0100

    Maj v_depot.php
    Ajout de la liste complète

--- view/v_depot.php
+++ view/v_depot.php
@@ -9,7 +9,55 @@
 
 </head>
 
+<style>
+    
+/* Dropdown Button */
+.dropbtn {
+  background-color: #81D0E4;
+  color: black;
+  padding: 16px;
+  font-size: 16px;
+  font-weight: bold;
+  border: none;
+  cursor: pointer;
+}
+
+/* Dropdown button on hover & focus */
+.dropbtn:hover, .dropbtn:focus {
+  background-color: #81D0E4;
+}
+
+/* The container <div> - needed to position the dropdown content */
+.dropdown {
+  position: relative;
+  display: inline-block;
+}
+
+/* Dropdown Content (Hidden by Default) */
+.dropdown-content {
+  display: none;
+  position: absolute;
+  background-color: lightgray;
+  min-width: 160px;
+  box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+}
+
+/* Links inside the dropdown */
+.dropdown-content a {
+  color: black;
+  padding: 12px 16px;
+  text-decoration: none;
+  display: block;
+}
+
+/* Change color of dropdown links on hover */
+.dropdown-content a:hover {background-color: #f1f1f1}
+
+/* Show the dropdown menu (use JS to add this class to the .dropdown-content container when the user clicks on the dropdown button) */
+.show {display:block;}
+</style>
 <body>
+    <hr>
     <nav>
         <div class="fond depot-fond">
             <div id="zen">
@@ -21,22 +69,143 @@
         <hr id="lignedepot"> 
             <nav>
                 <div>
-                <span> Examen/concours - Bac technologique </span>
-                <button>Déposer</button>
-                <br>
+                    <div class="fond-2 depot-fond">
+                        <span id="texte"> Examen/concours - Bac technologique </span>
+                        <button class="boutons depots titre dépots">Déposer</button>
+                        <div class="dropdown">
+                            <button onclick="myFunction()" class="dropbtn">⇩</button>
+                            <div id="myDropdown" class="dropdown-content">
+                                <span class="depottexte">Nom du fichier</span><br>
+                                <span class="depottexte">Date de début de publication</span><br>
+                                <span class="depottexte">Date de fin de publication</span>
+                            </div>
+                            <script>/* When the user clicks on the button,
+                            toggle between hiding and showing the dropdown content */
+                            function myFunction() {
+                                document.getElementById("myDropdown").classList.toggle("show");
+                                }
 
-                <span> Examen/concours - Bac Général </span>
-                <button>Déposer</button>
-                <br>
+                                // Close the dropdown menu if the user clicks outside of it
+                            window.onclick = function(event) {
+                                if (!event.target.matches('.dropbtn')) {
 
-                <span> Examen/concours - Épreuves Anticipées </span>
-                <button>Déposer</button>
-                <br>
+                                    var dropdowns = document.getElementsByClassName("dropdown-content");
+                                    var i;
+                                    for (i = 0; i < dropdowns.length; i++) {
+                                        var openDropdown = dropdowns[i];
+                                        if (openDropdown.classList.contains('show')) {
+                                        openDropdown.classList.remove('show');
+                                    }
+                                    }
+                                }
+                                }
+                            </script>
+                        </div>
+                    </div>
+                    <br>
 
+                    <div class="fond-2 depot-fond">
+                        <span id="texte"> Examen/concours - Bac Général </span>
+                        <button class="boutons depots titre dépots">Déposer</button>
+                        <div class="dropdown">
+                            <button onclick="myFunction()" class="dropbtn">⇩</button>
+                            <div id="myDropdown" class="dropdown-content">
+                                <span class="depottexte">Nom du fichier</span><br>
+                                <span class="depottexte">Date de début de publication</span><br>
+                                <span class="depottexte">Date de fin de publication</span>
+                            </div>
+                            <script>/* When the user clicks on the button,
+                            toggle between hiding and showing the dropdown content */
+                            function myFunction() {
+                                document.getElementById("myDropdown").classList.toggle("show");
+                                }
 
-                <span> Examen/concours - Bac Professionnel </span>
-                <button>Déposer</button>
-                </div>
+                                // Close the dropdown menu if the user clicks outside of it
+                            window.onclick = function(event) {
+                                if (!event.target.matches('.dropbtn')) {
+
+                                    var dropdowns = document.getElementsByClassName("dropdown-content");
+                                    var i;
+                                    for (i = 0; i < dropdowns.length; i++) {
+                                        var openDropdown = dropdowns[i];
+                                        if (openDropdown.classList.contains('show')) {
+                                        openDropdown.classList.remove('show');
+                                    }
+                                    }
+                                }
+                                }
+                            </script>
+                        </div>
+                    </div>
+                    <br>
+                    <div class="fond-2 depot-fond">
+                        <span id="texte"> Examen/concours - Épreuves anticipé</span>
+                        <button class="boutons depots titre dépots">Déposer</button>
+                        <div class="dropdown">
+                            <button onclick="myFunction()" class="dropbtn">⇩</button>
+                            <div id="myDropdown" class="dropdown-content">
+                                <span class="depottexte">Nom du fichier</span><br>
+                                <span class="depottexte">Date de début de publication</span><br>
+                                <span class="depottexte">Date de fin de publication</span>
+                            </div>
+                            <script>/* When the user clicks on the button,
+                            toggle between hiding and showing the dropdown content */
+                            function myFunction() {
+                                document.getElementById("myDropdown").classList.toggle("show");
+                                }
+
+                                // Close the dropdown menu if the user clicks outside of it
+                            window.onclick = function(event) {
+                                if (!event.target.matches('.dropbtn')) {
+
+                                    var dropdowns = document.getElementsByClassName("dropdown-content");
+                                    var i;
+                                    for (i = 0; i < dropdowns.length; i++) {
+                                        var openDropdown = dropdowns[i];
+                                        if (openDropdown.classList.contains('show')) {
+                                        openDropdown.classList.remove('show');
+                                    }
+                                    }
+                                }
+                                }
+                            </script>
+                        </div>
+                    </div>
+                    <br>
+                    <div class="fond-2 depot-fond">
+                        <span id="texte"> Examen/concours - Bac professionnel </span>
+                        <button class="boutons depots titre dépots">Déposer</button>
+                        <div class="dropdown">
+                            <button onclick="myFunction()" class="dropbtn">⇩</button>
+                            <div id="myDropdown" class="dropdown-content">
+                                <span class="depottexte">Nom du fichier</span><br>
+                                <span class="depottexte">Date de début de publication</span><br>
+                                <span class="depottexte">Date de fin de publication</span>
+                            </div>
+                            <script>
+
+                            function myFunction() {
+                                document.getElementById("myDropdown").classList.toggle("show");
+                                }
+
+                                // Close the dropdown menu if the user clicks outside of it
+                            window.onclick = function(event) {
+                                if (!event.target.matches('.dropbtn')) {
+
+                                    var dropdowns = document.getElementsByClassName("dropdown-content");
+                                    var i;
+                                    for (i = 0; i < dropdowns.length; i++) {
+                                        var openDropdown = dropdowns[i];
+                                        if (openDropdown.classList.contains('show')) {
+                                        openDropdown.classList.remove('show');
+                                    }
+                                    }
+                                }
+                                }
+                            </script>
+                        </div>
+                    </div>
+                    <br>
 
 
             </nav>
