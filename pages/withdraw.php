fa22bac75253962ce8cb19372de15dfccdc7250a
Corentin-BREANT <84771683+Corentin-BREANT@users.noreply.github.com>
2022-02-11 15:55:47 +0100

    Ajout de boutons pour télécharger

--- pages/withdraw.php
+++ pages/withdraw.php
@@ -8,24 +8,25 @@
 
 <h1>ESPACE RETRAIT</h1>
 
+
 <div id="container-wrapper">
     <?php
     // Pour chaque type d'épreuve/concours, on ajoute un
     // nouvel élément permettant l'accès à l'espace
     // d'échange.
 
-    foreach ($typesEpreuves as $index => $type) { ?>
+    foreach ($typesEpreuves as $index => $type) { 
+        ?>
         <div class="exchange-wrapper">
             <div class="container dropdown-button" onclick="dropdownHandler('<?= $index; ?>')">
                 <h3 class="dropdown-button">Examen/concours - <?= $type; ?></h3>
             </div>
             <div class="exchange-zone" id="exchange-zone-<?= $index; ?>">
-
             </div>
+        <?php
+         } 
+         ?>
         </div>
-
-    <?php } ?>
-</div>
 
 <script src="/public/javascript/dropdown.js"></script>
 
